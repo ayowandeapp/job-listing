9aa0751efe4744a7bc24a38383f3be7244d8f808
Ayowande Oluwatosin <ayooluwa71@gmail.com>
2024-12-28 23:02:34 +0100

    added views files

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -2,13 +2,10 @@
 <?= loadPartials('header') ?>
 <?= loadPartials('showcase-search') ?>
 <?= loadPartials('top-banner') ?>
-<!-- Top Banner -->
 
-
-<!-- Job Listings -->
 <section>
     <div class="container mx-auto p-4 mt-4">
-        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
+        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">All Jobs</div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             <!-- Job Listing 1: Software Engineer -->
             <div class="rounded-lg shadow-md bg-white">
@@ -140,6 +137,7 @@
                 </div>
             </div>
 
+            <!-- Job Listing 6: Data Scientist -->
             <div class="rounded-lg shadow-md bg-white">
                 <div class="p-4">
                     <h2 class="text-xl font-semibold">Data Scientist</h2>
@@ -165,10 +163,6 @@
                 </div>
             </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-            <i class="fa fa-arrow-alt-circle-right"></i>
-            Show All Jobs
-        </a>
 </section>
 
 
